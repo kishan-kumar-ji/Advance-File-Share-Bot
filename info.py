be9da691356c427e61903df660ce669b6e70f71c
kishan-kumar-ji <158287711+kishan-kumar-ji@users.noreply.github.com>
2024-08-23 22:08:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ PICS = (environ.get('PICS', 'https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg'
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6955808772 920827971 2080522555 7266232841').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001900707992').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1251111009').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP','None')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
